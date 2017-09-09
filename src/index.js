4c8808c59a8ed78dd59db7f8cb57603e6ece5d1e
Les Orchard <me@lmorchard.com>
2017-09-09 02:13:54 -0400

    Small tweaks

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import hello from './lib/core';
+var hello = require('./lib/core').default;
 
 import './index.css';
 
-const systems = { hello };
+const world = {};
 
 let count = 0;
 function update () {
   count++;
-  document.getElementById('title').innerText = `${systems.hello()} ${count}`;
+  document.getElementById('title').innerText = `${hello()} ${count}`;
   setTimeout(update, 16);
 }
 setTimeout(update, 16);
 
 if (module.hot) {
   module.hot.accept('./lib/core.js', function (loaded) {
-    systems.hello = require('./lib/core.js').default;
+    hello = require('./lib/core.js').default;
   });
 }
